9010ad775bb476cd1d4c4f10ffcbae76e581a587
Reeberth <132923081+reeberth@users.noreply.github.com>
2023-11-27 10:50:18 -0300

    Update script.js

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -97,9 +97,9 @@ function changeWinner(square) {
 }
 
 function changesColorSquare(square1, square2, square3) {
-    square1.style.background = '#0f0';
-    square2.style.background = '#0f0';
-    square3.style.background = '#0f0';
+    square1.style.background = '#F0E68C';
+    square2.style.background = '#F0E68C';
+    square3.style.background = '#F0E68C';
 }
 
 function checkSequence(square1, square2, square3) {
